a9de9e2bdc95be47e59154d0cc98e5f8f68401d6
Millie <jay.lee.2280@gmail.com>
2022-08-25 22:47:12 +0900

    feat: remove list msw

--- src/_msw/handler.ts
+++ src/_msw/handler.ts
@@ -1,13 +1,6 @@
 import { commentHandler } from './Comment';
-import { listHandler } from './list';
 import { myHandler } from './My';
 import { postHandler } from './Posts';
 import { validationHandler } from './Validation';
 
-export const handlers = [
-  ...postHandler,
-  ...myHandler,
-  ...listHandler,
-  ...validationHandler,
-  ...commentHandler,
-];
+export const handlers = [...postHandler, ...myHandler, ...validationHandler, ...commentHandler];
